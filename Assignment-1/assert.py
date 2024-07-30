7ab3da43a838b917ef6cd0372c68b3b21d846b4c
5hir-shoT <shirshotarafdar@gmail.com>
2024-07-30 08:46:00 +0530

    assertionin python

---
+++ Assignment-1/assert.py
@@ -0,0 +1,8 @@
+def assert_non_empty(numbers):
+    assert len(numbers) > 0, "The list is empty"
+
+# Example usage
+numbers = [1, 2, 3, 4, 5]
+assert_non_empty(numbers)
+
+# No output will be produced

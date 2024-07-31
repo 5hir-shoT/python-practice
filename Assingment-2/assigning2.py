148435e5d875410aa3f1b848d2eefe9937946421
5hir-shoT <shirshotarafdar@gmail.com>
2024-07-31 15:08:21 +0530

    name || year || height || is_ok || area

---
+++ Assingment-2/assigning2.py
@@ -0,0 +1,17 @@
+name="Shirsho"
+year=2024
+height=5.7
+is_ok=True
+areaofcircle=2+3j
+
+print("Name: ",name)
+print("Year: ",year)
+print("Height: ",height)
+print("Is he ok?", is_ok)
+print("Area of circle: ",areaofcircle)
+
+#Name:  Shirsho
+#Year:  2024
+#Height:  5.7
+#Is he ok? True
+#Area of circle:  (2+3j)

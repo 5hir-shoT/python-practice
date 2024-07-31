4465152e3e0722a68d066f0bf92ea971f74fa98d
5hir-shoT <shirshotarafdar@gmail.com>
2024-07-31 15:45:43 +0530

    dictionary

---
+++ Assingment-2/dictionaries.py
@@ -0,0 +1,7 @@
+student={"Name:":"Shirako",
+         "Age:":18,
+         "courses:":["Mathematics", "Physics", "Computer Science"]}
+print(student)
+
+student["graduated"] = False
+print("Updated dictionary:\n",student)

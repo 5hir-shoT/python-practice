6466a73063d1f59be430c1c39873dec4fba3a43c
5hir-shoT <shirshotarafdar@gmail.com>
2024-08-07 15:21:22 +0530

    bitwise operator

---
+++ Assingment-3/bitwise.py
@@ -0,0 +1,18 @@
+a=int(input("a="))
+b=int(input("b="))
+
+print("a & b=",a&b)
+print("a | b=",a|b)
+print("a ^ b=",a^b)
+print("~a=",~a)
+print("a << 1=",a<<1)
+print("a >> 1=",a>>1)
+
+#a=5
+#b=3
+#a & b= 1
+#a | b= 7
+#a ^ b= 6
+#~a= -6
+#a << 1= 10
+#a >> 1= 2

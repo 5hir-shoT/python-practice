2ab6c330e5a9c96e20bd048f26afd01fe577aaa7
5hir-shoT <shirshotarafdar@gmail.com>
2024-08-07 14:25:02 +0530

    arithmetic operations

---
+++ Assingment-3/arithmetic_operators.py
@@ -0,0 +1,28 @@
+num1=float(input("enter number 1="))
+num2=float(input("enter number 2="))
+
+add=num1+num2   #addition
+sub=num1-num2   #subtraction
+mul=num1*num2   #multiplication
+div=num1/num2   #division
+mod=num1%num2   #modulus
+expo=num1**num2 #exponentiation
+flo=num1//num2  #floor division
+
+print(f"a+b={add}")
+print(f"a-b={sub}")
+print(f"a*b={mul}")
+print(f"a/b={div}")
+print(f"a%b={mod}")
+print(f"a**b={expo}")
+print(f"a//b={flo}")
+
+#enter number 1=15
+#enter number 2=4
+#a+b=19.0
+#a-b=11.0
+#a*b=60.0
+#a/b=3.75
+#a%b=3.0
+#a**b=50625.0
+#a//b=3.0

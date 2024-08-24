9e67c6dc5004daae0c8a29afaefce2b6d1097690
5hir-shoT <shirshotarafdar@gmail.com>
2024-08-24 09:18:42 +0530

    best factorial method

--- Assingment-4/factorial_function.py
+++ Assingment-4/factorial_function.py
@@ -7,3 +7,8 @@ def factorial(n):
 n = int(input("Enter a non-negative integer: "))
 result = factorial(n)
 print("The factorial of", n, "is:", result)
+
+
+#   Output:
+#   Enter a non-negative integer: 5
+#   The factorial of 5 is: 120

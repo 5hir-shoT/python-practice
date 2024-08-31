0fcb482e8c6bcfb361c28c46e1c9ef431618cc1e
5hir-shoT <shirshotarafdar@gmail.com>
2024-08-31 08:01:25 +0530

    Rectangle area and perimeter calculation using class/ object || (5.2)

--- Assignment-5/rectangle_ar_peri_classobject.py
+++ Assignment-5/rectangle_ar_peri_classobject.py
@@ -16,6 +16,7 @@ rectangle = Rectangle(5.0, 3.0)
 print(f"Area: {rectangle.area()}")
 print(f"Perimeter: {rectangle.perimeter()}")
 
+
 # --------------------------------------------------
 
 #   Output:
